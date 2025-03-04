49d2aaecc6e50fa80bfd02a844478ba103b3ce86
Madina-Kudanova <importantbin@gmail.com>
2025-03-03 22:04:20 -0500

    Update sort_compare.py

--- sort_compare.py
+++ sort_compare.py
@@ -1,43 +1,46 @@
-import random
 import time
+import random
 
-def get_me_random_list(n):
-    return [random.randint(1, 1000000) for _ in range(n)]
+def get_random_list(n):
+    """Generate a randomized list containing n elements"""
+    lst = list(range(n))
+    random.shuffle(lst)
+    return lst
 
-def insertion_sort(a_list):
+def insertion_sort(lst):
     start_time = time.time()
-    for index in range(1, len(a_list)):
-        current_value = a_list[index]
+    for index in range(1, len(lst)):
+        current_value = lst[index]
         position = index
-        while position > 0 and a_list[position - 1] > current_value:
-            a_list[position] = a_list[position - 1]
+        while position > 0 and lst[position - 1] > current_value:
+            lst[position] = lst[position - 1]
             position -= 1
-        a_list[position] = current_value
+        lst[position] = current_value
     end_time = time.time()
     return end_time - start_time
 
-def shell_sort(a_list):
+def shell_sort(lst):
     start_time = time.time()
-    gap = len(a_list) // 2
+    gap = len(lst) // 2
     while gap > 0:
         for start in range(gap):
-            gap_insertion_sort(a_list, start, gap)
+            gap_insertion_sort(lst, start, gap)
         gap //= 2
     end_time = time.time()
     return end_time - start_time
 
-def gap_insertion_sort(a_list, start, gap):
-    for i in range(start + gap, len(a_list), gap):
-        current_value = a_list[i]
+def gap_insertion_sort(lst, start, gap):
+    for i in range(start + gap, len(lst), gap):
+        current_value = lst[i]
         position = i
-        while position >= gap and a_list[position - gap] > current_value:
-            a_list[position] = a_list[position - gap]
+        while position >= gap and lst[position - gap] > current_value:
+            lst[position] = lst[position - gap]
             position -= gap
-        a_list[position] = current_value
+        lst[position] = current_value
 
-def python_sort(a_list):
+def python_sort(lst):
     start_time = time.time()
-    a_list.sort()
+    lst.sort()
     end_time = time.time()
     return end_time - start_time
 
@@ -46,7 +49,7 @@ def benchmark_sort(size, num_tests=100):
     shell_times = []
     python_sort_times = []
     for _ in range(num_tests):
-        lst = get_me_random_list(size)
+        lst = get_random_list(size)
         insertion_times.append(insertion_sort(lst[:]))
         shell_times.append(shell_sort(lst[:]))
         python_sort_times.append(python_sort(lst[:]))
@@ -62,4 +65,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
